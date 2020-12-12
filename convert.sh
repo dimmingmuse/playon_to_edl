6a1a255958dca40cb408a65d0935714a8108c477
dimmingmuse <dimmingmuse.20.ice@spamgourmet.com>
2020-12-11 21:59:01 -0800

    Only create EDL files if they don't exist

--- convert.sh
+++ convert.sh
@@ -3,13 +3,15 @@
 
 filename=$(echo $1)
 fname="${filename%.*}"
-out_fname="${fname}.txt"
+temp_fname="${fname}.txt"
 edl_fname="${fname}.edl"
 
-echo "converting to ${out_fname}"
 
-ffmpeg -y -i "${filename}" -f ffmetadata "${out_fname}"
-
-cat "${out_fname}" | python3 ~/convert.py > "${edl_fname}"
-
-rm "${out_fname}"
+if [ -f "$edl_fname" ]; then
+    echo "$edl_fname already exists."
+else
+    echo "$edl_fname does not exist. Converting."
+    ffmpeg -y -i "${filename}" -f ffmetadata "${temp_fname}"
+    cat "${temp_fname}" | python3 ~/convert.py > "${edl_fname}"
+    rm "${temp_fname}"
+fi
